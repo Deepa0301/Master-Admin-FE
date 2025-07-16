05bbc88608f1c42204c9a297b1ffed5ab947a048
Deepa <deepa@e-arth.in>
2025-07-16 21:21:22 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-600 font-semibold'>Welcome to Master Admin site</h1>
+      <Button className='mt-5'>Hello</Button>
+    </>
+  )
+}
+
+export default App
